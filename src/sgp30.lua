964d9fd053c502a65e16d35d29ade184aca8aa9f
David Rauschenbach <davidr@imap.cc>
2019-05-14 06:40:52 -0700

    Support requesting feature set

--- src/sgp30.lua
+++ src/sgp30.lua
@@ -1,10 +1,23 @@
+local I2C = require 'periphery'.I2C
+
 local M = {
+  Command = {
+    GetFeatureSet  = {0x20, 0x2f},
+    InitAirQuality = {0x20, 0x03}
+  },
   DEVICE = 0x58
 }
 
 function M.initAirQuality(i2c, device)
-  local msgs = {{0x20, 0x03}}
+  local msgs = {M.Command.InitAirQuality}
   i2c:transfer(device or M.DEVICE, msgs)
+end
+
+function M.readVersion(i2c, device)
+  local msgs = {M.Command.GetFeatureSet, {0x00, 0x00, 0x00, flags=I2C.I2C_M_RD}}
+  i2c:transfer(device or M.DEVICE, msgs)
+  local lsb = msgs[2][2]
+  return lsb
 end
 
 return M
